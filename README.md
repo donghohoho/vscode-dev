1d6a9bc324a131e57b30c7511c3bab03800afdbe
donghohoho <donghosim61@gmail.com>
2025-12-17 11:59:50 +0900

    수정함:        README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # vscode-dev
 Coding training 
+
+hello mates
+
+i'm going to test git push
+
+HELLOOOOOO!!!!!
+
--- new.cpp
+++ new.cpp
@@ -2,11 +2,24 @@
 # include <iostream>
 inlcude namespace:std;
 
+Class Hello {
+private
+    int _a;
+    int _b;
+public
+    int _c;
+    int _d;
+    s
+    
+};
+
 int main{
-int a = 10;
-  int b = 20;
-  cout << a << b << endl;
+    int a = 10;
+    int b = 20;
+    cout << a << b << endl;
 
 
 
 }
+
+
